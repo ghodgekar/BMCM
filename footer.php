772ed2099c11b14a52cbf38c183d824f70dd01d9
ghodgekar <surabhighodgekar30@gmail.com>
2023-11-19 11:55:30 +0530

    changes updated

--- footer.php
+++ footer.php
@@ -24,3 +24,4 @@
 <script src="assets/js/jquery.validate.js"></script>
 <script src="assets/js/bootstrap.bundle.min.js"></script>
 <script src="assets/js/main.js"></script>
+<script src="//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
